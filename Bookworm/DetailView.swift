8c424914797c15b33a666215eb27511456aa7deb
slavabily <slavabily@me.com>
2020-04-15 18:22:24 +0300

    adding date of book review to the Detail View

--- Bookworm/DetailView.swift
+++ Bookworm/DetailView.swift
@@ -27,11 +27,11 @@ struct DetailView: View {
                     Text(self.book.genre?.uppercased() ?? "FANTASY")
                         .font(.caption)
                         .fontWeight(.black)
-                    .padding(8)
+                        .padding(8)
                         .foregroundColor(.white)
                         .background(Color.black.opacity(0.75))
-                    .clipShape(Capsule())
-                    .offset(x: -5, y: -5)
+                        .clipShape(Capsule())
+                        .offset(x: -5, y: -5)
                 }
                 
                 Text(self.book.author ?? "Unknown author")
@@ -43,6 +43,11 @@ struct DetailView: View {
                 
                 RatingView(rating: .constant(Int(self.book.rating)))
                     .font(.largeTitle)
+                
+                Text(self.currentDateFormatted())
+                    .font(.footnote)
+                    .padding()
+                    .foregroundColor(.secondary)
                 
                 Spacer()
             }
@@ -59,6 +64,18 @@ struct DetailView: View {
         }, label: {
             Image(systemName: "trash")
         }))
+    }
+    
+    func currentDateFormatted() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm E, d MM y"
+        
+        if let date = book.date {
+            print(date)
+            return formatter.string(from: date)
+        } else {
+            return "No Review Date"
+        }
     }
     
     func deleteBook() {
@@ -82,6 +99,7 @@ struct DetailView_Previews: PreviewProvider {
         book.genre = "Fantasy"
         book.rating = 4
         book.review = "This was a grait book; I really enjoyed it."
+        book.date = Date()
         
         return NavigationView {
             DetailView(book: book)
